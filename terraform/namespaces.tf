d14db8340e266949eb92e07d5f39e2642e9ab8ee
Gary Aju <garyacdev@gmail.com>
2026-09-12 13:01:41 -0600

    ADD: Namespace v1 added

---
+++ terraform/namespaces.tf
@@ -0,0 +1,22 @@
+locals {
+  namespaces = {
+    argocd         = "gitops"
+    argo-rollouts  = "progressive-delivery"
+    kyverno        = "policy-enforcement"
+    sealed-secrets = "encrypted-secrets"
+    sa-p8          = "mediconnect-production"
+  }
+}
+
+resource "kubernetes_namespace_v1" "managed" {
+    for_each = locals.namespaces
+
+    metadata {
+        name = each.key
+
+        labels = {
+            "app.kubernetes.io/managed-by"  = "terraform"
+            "mediconnect.io/purpose"        = each.value
+        }
+    }
+}
